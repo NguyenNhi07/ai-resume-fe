d729699c40f67ece65d09eff3a7d6e2715f31bfa
NguyenNhi07 <nguyennhi.hau2003@gmail.com>
2025-10-03 10:48:49 +0700

    feat: base

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/auth/sign-in',
+    element: <SignInPage/>
+  }
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
