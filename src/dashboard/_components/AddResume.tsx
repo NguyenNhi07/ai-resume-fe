7fc41aaa4ef06330868327481d925e4e300bd39c
NguyenNhi07 <nguyennhi.hau2003@gmail.com>
2025-10-13 16:27:22 +0700

    feat: create new resume

---
+++ src/dashboard/_components/AddResume.tsx
@@ -0,0 +1,72 @@
+import { Loader2, PlusSquare } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { v4 as uuidv4 } from 'uuid'
+import GlobalApi from "../../../service/GlobalApi";
+import { useUser } from "@clerk/clerk-react";
+
+export function AddResume() {
+    const [openDialog, setOpenDialog] = useState(false)
+    const [resumeTitle, setResumeTitle] = useState()
+    const [loading, setLoading] = useState(false)
+    const { user } = useUser()
+
+    const onCreate = () => {
+        setLoading(true)
+        const uuid = uuidv4()
+
+        const data = {
+            data: {
+                title: resumeTitle,
+                resumeId: uuid,
+                userEmail: user?.primaryEmailAddress?.emailAddress,
+                userName: user?.fullName
+            }
+        }
+
+        GlobalApi.CreateNewResume(data).then(reponse => {
+            console.log(reponse)
+            if (reponse) {
+                setLoading(false)
+            }
+        }, (error) => {
+            console.log(error)
+            setLoading(false)
+        })
+        setOpenDialog(false)
+    }
+
+    return (
+        <div>
+            <div className="p-14 py-24 border items-center flex justify-center bg-secondary rounded-lg h-[280px] hover:scale-105 transition-all hover:shadow-md cursor-pointer border-dashed"
+                onClick={() => setOpenDialog(true)}
+            >
+                <PlusSquare />
+            </div>
+
+            <Dialog open={openDialog}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Create New Resume</DialogTitle>
+                        <DialogDescription>
+                            <p>Add a title for your new resume</p>
+                            <Input onChange={(e: any) => setResumeTitle(e.target.value)} className="my-2" placeholder="Ex.Full Stack resume" />
+                        </DialogDescription>
+                        <div className="flex justify-end gap-4">
+                            <Button onClick={() => setOpenDialog(false)} variant='ghost'>Cancel</Button>
+                            <Button disabled={!resumeTitle || loading} onClick={onCreate}>{loading ? <Loader2 className="animate-spin" /> : 'Create'}</Button>
+                        </div>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
+}
--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -1,8 +1,13 @@
+import { AddResume } from "./_components/AddResume";
+
 export default function Dashboard () {
     return <>
-        <div className="p-8">
-            <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-            <p>Welcome to your dashboard!</p>
+        <div className="p-10 md:px-20 lg:px-32">
+            <h2 className="font-bold text-3xl">My Resume</h2>
+            <p>Start Creating AI resume to your next Job role</p>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10">
+                <AddResume/>
+            </div>
         </div>
     </>
 }
